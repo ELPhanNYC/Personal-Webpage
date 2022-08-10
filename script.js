90dbd7e47e7930e254c5bb07940d43517a3300c7
elphan <starsoccerethan@gmail.com>
2022-08-10 15:19:09 -0400

    Created a timeline in exp.html to display experiences in more attractive manner. Updated resume and JS Script for new div in edu.html.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -14,19 +14,26 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+
+}
+
+function displayInfo(bool){
+  let info = document.getElementById("reveal_button")
+  let info_close = document.getElementById("reveal")
+  if(bool == true){
+    info.style.display = "none";
+    info_close.style.display = "inline-block";
+  }
+  else{
+    info.style.display = "inline-block";
+    info_close.style.display = "none";
+  }
 }
 
 
